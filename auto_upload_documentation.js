50989a676dd3e5069da37292f7d39f215ec22a48
Tim <stantim@hotmail.com>
2020-02-24 20:49:21 +0100

    V1.0

---
+++ auto_upload_documentation.js
@@ -0,0 +1,62 @@
+const { exec } = require('child_process');
+require('dotenv').config();
+
+var date = new Date();
+
+//github credentials and repo information
+const USER = process.env.GITHUB_USERNAME;
+const PASS = process.env.GITHUB_PASSWORD;
+
+const REPO_NAME = 'input_repo_name'
+const REPO_LINK = `github.com/input_username_of_git_repo/${REPO_NAME}.git`;
+
+//check if all required fields are setup by the user
+if (USER == 'input_your_username_here' ||
+    PASS == 'input_your_password_here' ||
+    REPO_NAME == 'input_repo_name' ||
+    REPO_LINK.contains("input_username_of_git_repo")) {
+
+    console.error("One of the following variables aren't setup: USER, PASS, REPO_NAME, REPO_LINK")
+    process.exit();
+}
+
+//create a directory with the name of the repository (if it doesn't exist)
+exec('mkdir -p ' + REPO_NAME, (err, stdout) => {
+    if (err) {
+        //some err occurred
+        console.error(err)
+    } else {
+        // the *entire* stdout (buffered)
+        console.log(`${stdout}`);
+    }
+});
+
+//setup git tools
+const git = require('simple-git/promise')(__dirname + '/' + REPO_NAME);
+const remote = `https://${USER}:${PASS}@${REPO_LINK}`;
+
+//function which clones the repository if it doesn't exist yet
+function initialiseRepo(git) {
+    console.log("setting up the git repo: " + REPO_LINK);
+    return git.clone(remote, __dirname + '/' + REPO_NAME)
+        .then(() => console.log('finished'))
+        .catch((err) => console.error('failed: ', err));
+}
+
+main structure which handles the pull (for being up to date), and pushing the new documentation
+git.checkIsRepo()
+    .then(isRepo => !isRepo && initialiseRepo(git))
+    .then(() => git.pull(remote, "master"))
+    .catch((err) => console.error('failed: ', err))
+    .then(() => exec('mkdir -p ' + REPO_NAME + '/doc && cp -r Onedrive_link/* ' + REPO_NAME + '/doc', (err, stdout) => {
+        if (err) {
+            //some err occurred
+            console.error(err)
+        } else {
+            // the *entire* stdout (buffered)
+            console.log(`${stdout}`);
+        }
+    }))
+    .then(() => git.add('./*'))
+    .then(() => git.commit("Documentation commit " + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()))
+    .then(() => git.push())
