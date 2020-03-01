5c1bae48eda00b7ba9a0aa335ef820d284d7b41a
Tim <stantim@hotmail.com>
2020-03-01 23:08:28 +0100

    removed some debug logs

--- auto_upload_documentation.js
+++ auto_upload_documentation.js
@@ -15,14 +15,14 @@ const REPO_NAME = process.env.REPO_NAME;
 const REPO_LINK = process.env.REPO_LINK;
 
 //check if all required fields are setup by the user
-// if (USER == 'input_your_username_here' ||
-//     PASS == 'input_your_password_here' ||
-//     REPO_NAME == 'input_repo_name' ||
-//     REPO_LINK.indexOf("input_username_of_git_repo") != -1) {
+if (USER == 'input_your_username_here' ||
+    PASS == 'input_your_password_here' ||
+    REPO_NAME == 'input_repo_name' ||
+    REPO_LINK.indexOf("input_username_of_git_repo") != -1) {
 
-//     console.error("One of the following variables aren't setup: USER, PASS, REPO_NAME, REPO_LINK")
-//     process.exit();
-// }
+    console.error("One of the following variables aren't setup: USER, PASS, REPO_NAME, REPO_LINK")
+    process.exit();
+}
 
 //create a directory with the name of the repository (if it doesn't exist)
 exec('mkdir -p ' + REPO_NAME, (err, stdout) => {
@@ -38,20 +38,20 @@ exec('mkdir -p ' + REPO_NAME, (err, stdout) => {
 //setup git tools
 const git = require('simple-git/promise')(__dirname + '/' + REPO_NAME);
 const remote = `https://${USER}:${PASS}@${REPO_LINK}`;
-console.log(`using the following remote link: ${remote}`)
+// console.log(`using the following remote link: ${remote}`)
 //function which clones the repository if it doesn't exist yet
 function initialiseRepo(git) {
     console.log("setting up the git repo: " + REPO_LINK);
     return git.clone(remote, __dirname + '/' + REPO_NAME)
-        .then(() => console.log('finished'))
-        .catch((err) => console.error('failed: ', err));
+        .then(() => console.log('finished setting up the git repo'))
+        .catch((err) => console.error('failed setting up the git repo: ', err));
 }
 
 //main structure which handles the pull (for being up to date), and pushing the new documentation
 git.checkIsRepo()
     .then(isRepo => !isRepo && initialiseRepo(git))
     .then(() => git.pull(remote, "master"))
-    .catch((err) => console.error('failed: ', err))
+    .catch((err) => console.error('failed pulling: ', err))
     .then(() => exec('mkdir -p ' + REPO_NAME + '/doc && cp -r '+ PATH_TO_ONEDRIVE_DOCS +'/* ' + REPO_NAME + '/doc', (err, stdout) => {
         if (err) {
             //some err occurred
